da7b0af4843888f359bdbf021300d78c0e22d31e
juliawestlund <124053163+juliawestlund@users.noreply.github.com>
2023-05-19 13:27:19 +0200

    guide

--- KLC/KLC/Controllers/HomeController.cs
+++ KLC/KLC/Controllers/HomeController.cs
@@ -58,5 +58,10 @@ namespace KLC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult guide()
+        {
+            return View();
+        }
+
     }
 }
